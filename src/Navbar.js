d3bcab39e4acca60f7d03b95032fb1ceb41c8390
Harshal Madgulkar <harshal.madgulkar725@gmail.com>
2024-01-16 14:29:56 +0530

    Add CSS modules
    Convert styled component of Navbar into module.css files

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,63 +1,19 @@
 import React from "react";
-import styled from "styled-components";
-
-const Nav = styled.div`
-  width: 100%;
-  height: 80px;
-  background: linear-gradient(170deg, #1bc059, #0d47a1);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-`;
-
-const Title = styled.div`
-  font-size: 30px;
-  color: #fff;
-  font-weight: 600;
-  font-family: "Times New Roman", Times, Serif;
-  text-transform: uppercase;
-  margin-left: 20px;
-
-  &:hover{color: black};
-`;
-
-const CartIconContainer = styled.div`
-  position: relative;
-  cursor: pointer;
-`;
-
-const CartImg = styled.img`
-  height: 48px;
-  margin-right: 20px;
-`;
-
-const CartCount = styled.div`
-  background-color: ${(props) => props.color};
-  border-radius: 50%;
-  padding: 4px 8px;
-  position: absolute;
-  right: 10px;
-  top: -5px;
-  font-size: 12px;
-  visibility: ${(props) => (props.show ? "visible" : "hidden")};
-`;
+import NavStyle from "./navbar.module.css";
 
 class Navbar extends React.Component {
   render() {
     return (
-      <Nav>
-        <Title>MOVIE-APP</Title>
-        <CartIconContainer>
-          <CartImg
+      <div className={NavStyle.nav}>
+        <div className={NavStyle.title}>MOVIE-APP</div>
+        <div className={NavStyle.cartIconContainer}>
+          <img className={NavStyle.cartImage}
             alt="cart-icon"
             src="https://cdn-icons-png.flaticon.com/128/5542/5542671.png"
-          ></CartImg>
-          <CartCount color="yellow" show={true}>
-            4
-          </CartCount>
-        </CartIconContainer>
-      </Nav>
+          />
+          <span className={NavStyle.cartCount}>4</span>
+        </div>
+      </div>
     );
   }
 }
